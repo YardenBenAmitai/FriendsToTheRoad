145e96ab4fc8e8d04e48202eb491b994a27167aa
DenisAD1993 <denisadamovich12345@gmail.com>
2019-12-18 19:02:23 +0200

    Merge branch 'master' of C:\Users\denis1993\Desktop\App\FriendsToTheRoad with conflicts.

--- app/src/main/java/com/example/loginapplication/SignupActivity.java
+++ app/src/main/java/com/example/loginapplication/SignupActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -45,28 +46,44 @@ public class SignupActivity extends AppCompatActivity {
 
         fb_auth= FirebaseAuth.getInstance();
 
+
         execute.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (validate()){
                     //uploading users into database
                     fb_auth.createUserWithEmailAndPassword(email.getText().toString().trim(), password1.getText().toString().trim())
-                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if (task.isSuccessful()){
-                                UserProfile user=new UserProfile(
-                                        full_name.getText().toString().trim(),
-                                        date.getText().toString(),
-                                        country.getText().toString()
-                                );
-                                FirebaseDatabase.getInstance().getReference("Users")
-                                        .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
-                                        .setValue(user);
-                            }
-                            startActivity(new Intent(SignupActivity.this, IntroductionActivity.class));
-                        }
-                    });
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+
+
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()){
+                                        UserProfile user=new UserProfile(
+                                                full_name.getText().toString().trim(),
+                                                date.getText().toString(),
+                                                country.getText().toString()
+                                        );
+                                        FirebaseDatabase.getInstance().getReference("Users")
+                                                .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                                .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<Void> task) {
+                                                if(task.isSuccessful()) {
+                                                    Toast.makeText(SignupActivity.this, "Registered successfully", Toast.LENGTH_LONG).show();
+
+                                                    Intent intent1 = new Intent( SignupActivity.this , IntroductionActivity.class);
+                                                    startActivity(intent1);
+                                                }
+                                            }
+                                        });
+
+                                    } else{
+                                        Toast.makeText(SignupActivity.this, task.getException().getMessage(), Toast.LENGTH_LONG).show();
+
+                                    }
+                                }
+                            });
 
                 }
             }
@@ -84,7 +101,11 @@ public class SignupActivity extends AppCompatActivity {
                 info.setText("please fill out all the given fields");
             } else if(password1.getText().toString().trim().compareTo(password2.getText().toString().trim()) !=0){
                 info.setText("passwords don't match ");
-            } else{
+            }
+            else if((Integer.parseInt(date.getText().toString())<18)) {
+                info.setText("The minimum age to use the app is 18.");
+            }
+            else{
                 ans=true;
             }
             return ans;
